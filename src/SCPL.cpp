e12725cf8ec82019b72f8f00f9c69dae128720ab
Alexandr Iurchenko <104443262+bebre2288@users.noreply.github.com>
2022-06-25 22:31:35 +0500

    Update SCPL.cpp

--- src/SCPL.cpp
+++ src/SCPL.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <conio.h>
 
 using namespace std;
 
@@ -10,54 +11,63 @@ int temp = 0;
 
 int operate(int num) {
 	char oper = program[iter];
-	
-	switch(oper){
-		case '+':
-			num++;
-			break;
-		case '-':
-			num--;
-			break;
-		case '*':
-			iter++;
-			num *= operate(num);
-			break;
-		case '/':
-			iter++;
-			num /= operate(num);
-			break;
-		case '(':
-			temp = 0;
-			while(program[iter]!=')') {
-				iter++;
-				temp = operate(temp);
-			}
-			num = temp;
-			break;
-		case ',':
-			cout << (char)num;
-			num=0;
-			break;
-		case '.':
-			cin >> num;
-			break;
+	if(iter>program.length()){
+		return num;
 	}
+	if ( oper=='+'){
+		num++;
+	} else if ( oper=='-'){
+		num--;
+	} else if ( oper=='*'){
+		iter++;
+		num *= operate(num);
+	} else if ( oper=='/'){
+		iter++;
+		num /= operate(num);
+	} else if ( oper=='|'){
+		iter++;
+		num += operate(num);
+	} else if ( oper=='('){
+		temp = 0;
+		while(program[iter]!=')') {
+			iter++;
+			temp = operate(temp);
+		}
+		num = temp;
+	} else if ( oper==','){
+		cout << (char)num;
+		num=0;
+	} else if ( oper=='.'){
+		cin >> num;
+	}  else if ( oper==')'){
+		//pass
+	}  else {
+		iter++;
+		num=operate(num);
+	}
+
 	return num;
 }
 
 int main(int argc, char *argv[]) {
-	
+	string tempLine;
 	string fileName = argv[1];
 	ifstream file;
-	file.open (fileName, ios::in);
-	getline(file, program);
+	file.open (fileName);
+	while(!file.eof()){
+		getline(file, tempLine);
+		program += tempLine;
+	}
+	//cout << program << endl;
 	file.close();
-	cout << program << endl;
+
 	while(iter < program.length()) {
 		counter = operate(counter);
 		iter++;
+	
 	}
-	cout << "\nfinal: " << counter;
-	cin.get();
+	cout << endl;
+	cout << "final: " << counter << endl;
+	_getch();
 	return 0;
 }
